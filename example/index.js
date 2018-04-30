f7acef8715d12381db7efd42c6610abf11e64142
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-04-30 10:24:19 +0200

    Test2

--- example/index.js
+++ example/index.js
@@ -3,10 +3,10 @@ let interactive = process.argv[2] === "--interactive" || process.argv[2] === "-i
 const test = require("..")({outDir: __dirname+"/snapshots", interactive});
 
 (async function(){
-	await test("../libui/build/out/test", "Main Window", {raw: true});
+	// await test("../libui/build/out/test", "Main Window", {raw: true});
 
-	await test("../libui-node/examples/area-adv.js", "libui textDrawArea Example");
-	// await test("../libui-node/examples/text.js", "libui textDrawArea Example");
+	await test("area-adv.js", "libui textDrawArea Example");
+	await test("text.js", "libui textDrawArea Example");
 	// // await test("../libui-node/examples/core-api.js", "Test window");
 	// // await test("../libui-node/examples/control-gallery.js", "Control Gallery");
 	// // await test("../libui-node/examples/histogram.js", "libui Histogram Example");
